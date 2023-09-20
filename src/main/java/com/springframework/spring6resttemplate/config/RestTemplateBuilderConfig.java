8349684b339538cd0c69f4ed4b83b4e222644094
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2023-09-20 22:20:56 +0100

    OAuth2 implementation using authorized client manager, interceptor and OAuth2 Manager

--- src/main/java/com/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/com/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -5,27 +5,40 @@ import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConf
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class RestTemplateBuilderConfig {
 
-    @Value("${rest.template.username}")
-    String username;
-
-    @Value("${rest.template.password}")
-    String password;
-
     @Value("${rest.template.rootUrl}")
     String rootUrl;
 
     @Bean
-    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer config){
+    OAuth2AuthorizedClientManager auth2AuthorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                               OAuth2AuthorizedClientService oAuth2AuthorizedClientService){ //handle the call to the authorization server
+        var authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+
+        var authorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+    @Bean
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer config,
+                                            OAuthClientInterceptor authClientInterceptor){
 
         assert rootUrl != null;
 
         return config.configure(new RestTemplateBuilder())
-                .basicAuthentication(username, password)
+                .additionalInterceptors(authClientInterceptor)
                 .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl));
     }
 }
